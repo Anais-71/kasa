e57988d158603d3f86036aa01e576fbecc0f6a91
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-07-11 17:19:36 +0200

    feat: housing page

---
+++ src/pages/housing/Housing.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Dropdown from "../../components/dropdown/Dropdown"
+import Gallery from "../../components/gallery/Gallery"
+import { fetchData } from "../../utils/fetch";
+import { FaStar } from 'react-icons/fa'; 
+
+//CSS
+import './housing.css'
+
+function Housing() {
+    const [data, setData] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getData = async () => {
+            const dataJson = await fetchData();
+            const dataItem = dataJson.find(item => item.id === id);
+            setData(dataItem ? [dataItem] : []);
+        };
+        getData();
+    }, [id]);
+
+    return (
+        <div className="housing">
+            {data.map((item, index) => (
+                <div key={index}>
+                    <div className='housing__banner'>
+                    <Gallery images={item.pictures} />
+                    </div>
+                    <div className='housing__info'>
+                    <div className="housing__info--title">{item.title}</div>
+                    <div className="housing__info--location">{item.location}</div>
+                    </div>
+                    <div className="housing__host">
+                        <div className="housing__host--name">{item.host && item.host.name}</div>
+                        <img className="housing__host--picture" src={item.host && item.host.picture} />
+                    </div>
+                    <div className="housing__tags">
+                        {item.tags && item.tags.map((tag, i) => (
+                            <div key={i} className="housing__tags--tag">{tag}</div>
+                        ))}
+                    </div>
+                    <div className="housing__rating">
+                        {[...Array(5)].map((star, i) => {
+                            const ratingValue = i + 1;
+                            return (
+                                <FaStar key={i} color={ratingValue <= item.rating ? "red" : "#E3E3E3"} size={24} />
+                            );
+                        })}
+                    </div>
+                    <div className='housing__dropdowns'>
+                    <Dropdown title="Description" items={item.description ? [item.description] : []} />
+                    <Dropdown title="Équipements" items={item.equipments || []} />
+                </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Housing
---
+++ src/pages/housing/housing.test.js
@@ -0,0 +1,1 @@
+// Rédiger test d'intégration
