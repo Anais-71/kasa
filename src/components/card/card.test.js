325a64df79d8bd657225e776ddf1217eb3ac2e37
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-07-15 17:33:52 +0200

    test: card componenyt tests update

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -1,18 +1,25 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Card from './Card';
 
-describe('Given I am on the home page', () => {
-  const title = 'Test title';
-  const cover = ['https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg'];
+jest.mock('../../utils/fetch', () => ({
+  fetchData: () => Promise.resolve([
+    {
+      id: '1',
+      title: 'Test title',
+      cover: 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg'
+    }
+  ])
+}));
 
-  test('When the card component is being displayed, Then the title should be present', () => {
-    const { getByText } = render(<Card title={title} />);
-    expect(getByText(title)).toBeInTheDocument();
+describe('Given I am on the home page', () => {
+  test('When the card component is being displayed, Then the title should be present', async () => {
+    const { findByText } = render(<Card />);
+    expect(await findByText('Test title')).toBeInTheDocument();
   });  
 
-  test('When the card component is being displayed, Then the cover image should be present', () => {
-    const { getByText } = render(<Card cover={cover} />);
-    expect(getByText(cover)).toBeInTheDocument();
+  test('When the card component is being displayed, Then the cover image should be present', async () => {
+    const { findByRole } = render(<Card />);
+    expect(await findByRole('img', { name: /logement/i })).toBeInTheDocument();
   });
 
   test('The dropdown page matches the snapshot', () => {
