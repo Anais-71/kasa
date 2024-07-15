a43695497de38b70571a0473db70fb8e422bf2c7
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-07-15 17:16:00 +0200

    feat: routes updating

---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import Home from '../pages/home/Home';
+import About from '../pages/about/About';
+import Housing from '../pages/housing/Housing';
+import Error from '../components/error/Error';
+
+const Routes = [
+    { route: '/', element: <Home /> },
+    { route: '/about', element: <About /> },
+    { route: '/logement/:id', element: <Housing /> },
+    { route: '/*', element: <Error />},
+  ];
+  
+  export default Routes;
