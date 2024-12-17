77e18e1e7514f676729ccc5970dcafbefdb3c35e
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-12-17 10:49:55 +0100

    feat: deployement fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import routes from './routes/routes'; 
-import Layout from './layout/Layout';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import routes from './routes/routes'
+import Layout from './layout/Layout'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/kasa">
       <Layout>
-      <Routes>
-        {routes.map(({ route, element }, index) => (
-          <Route key={index} path={route} element={element} />
-        ))}
-      </Routes>
+        <Routes>
+          {routes.map(({ route, element }, index) => (
+            <Route key={index} path={route} element={element} />
+          ))}
+        </Routes>
       </Layout>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
